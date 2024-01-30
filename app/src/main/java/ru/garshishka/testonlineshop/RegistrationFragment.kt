144ed8f644ad20dbf9ffd6df60ce169f008cb811
garshishka <garshiv@gmail.com>
2024-01-30 23:23:29 +0300

    Initial commit. Added base registration fragment

---
+++ app/src/main/java/ru/garshishka/testonlineshop/RegistrationFragment.kt
@@ -0,0 +1,31 @@
+package ru.garshishka.testonlineshop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.garshishka.testonlineshop.databinding.FragmentRegistrationBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class RegistrationFragment : Fragment() {
+
+    private var _binding: FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+}
