8e2ce81bc35dbd371225c67e9b57faa2cd023678
garshishka <garshiv@gmail.com>
2024-01-31 23:13:37 +0300

    Added filter and sort buttons into catalogue fragment with icons and dropdown menu

--- app/src/main/java/ru/garshishka/testonlineshop/ui/CatalogueFragment.kt
+++ app/src/main/java/ru/garshishka/testonlineshop/ui/CatalogueFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
-import ru.garshishka.testonlineshop.R
+import com.google.android.material.chip.Chip
+import ru.garshishka.testonlineshop.databinding.FragmentCatalogueBinding
 
 class CatalogueFragment : Fragment() {
-
+    private var _binding: FragmentCatalogueBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,9 +19,35 @@ class CatalogueFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalogue, container, false)
+    ): View {
+        _binding = FragmentCatalogueBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+            if (checkedIds.isNotEmpty()) {
+                val selectedChip = group.findViewById<Chip>(checkedIds[0])
+                selectedChip.isCloseIconVisible = true
+                selectedChip.setOnCloseIconClickListener {
+                    selectedChip.isCloseIconVisible = false
+                    group.clearCheck()
+                }
+
+                for (chipId in group.children.map { it.id }) {
+                    if (chipId != checkedIds[0]) {
+                        group.findViewById<Chip>(chipId).isCloseIconVisible = false
+                    }
+                }
+            } else {
+                for (chipId in group.children.map { it.id }) {
+                    group.findViewById<Chip>(chipId).isCloseIconVisible = false
+                }
+            }
+        }
     }
 
 }
